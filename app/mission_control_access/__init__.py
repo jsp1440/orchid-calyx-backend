fa5ea55ff70d423c732a725b9b9ca6dd995a677f
Jeff Parham <jsp1440@aol.com>
2026-07-26 18:00:24 -0700

    MISSION-CONTROL-ROLE-001C export principal resolver contracts

--- app/mission_control_access/__init__.py
+++ app/mission_control_access/__init__.py
@@ -1,11 +1,15 @@
 from .models import AccessDecision, AccessPrincipal, Capability, MissionControlRole
+from .principals import AuthenticatedIdentity, PrincipalResolutionError, PrincipalResolver
 from .service import AccessDenied, CapabilityService
 
 __all__ = [
     "AccessDecision",
     "AccessDenied",
     "AccessPrincipal",
+    "AuthenticatedIdentity",
     "Capability",
     "CapabilityService",
     "MissionControlRole",
+    "PrincipalResolutionError",
+    "PrincipalResolver",
 ]
