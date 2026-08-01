058983053508f5dadd658b1a89f9bd6b32bf29ff
Jeff Parham <jsp1440@aol.com>
2026-07-31 18:04:39 -0700

    BUILD-080C add hardening rollback

---
+++ migrations/107_archive_production_hardening_rollback.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+DROP INDEX IF EXISTS archive_import_runs_lease_idx;
+DROP INDEX IF EXISTS archive_import_runs_status_idx;
+
+ALTER TABLE archive_import_runs DROP CONSTRAINT IF EXISTS archive_import_runs_status_check;
+ALTER TABLE archive_import_runs
+    ADD CONSTRAINT archive_import_runs_status_check
+    CHECK (status IN ('running','interrupted','completed','failed'));
+
+ALTER TABLE archive_import_runs
+    DROP COLUMN IF EXISTS dispatch_reference,
+    DROP COLUMN IF EXISTS lease_expires_at,
+    DROP COLUMN IF EXISTS lease_owner,
+    DROP COLUMN IF EXISTS heartbeat_at,
+    DROP COLUMN IF EXISTS attempt_count,
+    DROP COLUMN IF EXISTS cancel_requested;
+
+COMMIT;
