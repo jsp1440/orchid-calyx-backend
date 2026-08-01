3b18089eb441774170bb14f0ccc9320efa99e5de
Jeff Parham <jsp1440@aol.com>
2026-07-31 21:37:00 -0700

    BUILD-080C normalize statuses before rollback

--- migrations/107_archive_production_hardening_rollback.sql
+++ migrations/107_archive_production_hardening_rollback.sql
@@ -3,6 +3,15 @@ BEGIN;
 DROP INDEX IF EXISTS archive_import_runs_lease_idx;
 DROP INDEX IF EXISTS archive_import_runs_status_idx;
 
+UPDATE archive_import_runs
+SET status = CASE
+    WHEN status = 'queued' THEN 'interrupted'
+    WHEN status = 'cancelling' THEN 'interrupted'
+    WHEN status = 'cancelled' THEN 'failed'
+    ELSE status
+END
+WHERE status IN ('queued','cancelling','cancelled');
+
 ALTER TABLE archive_import_runs DROP CONSTRAINT IF EXISTS archive_import_runs_status_check;
 ALTER TABLE archive_import_runs
     ADD CONSTRAINT archive_import_runs_status_check
