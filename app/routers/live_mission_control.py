3aa8c212d1df82d443d2535098876ac7381a5844
Jeff Parham <jsp1440@aol.com>
2026-08-08 18:09:40 -0700

    CALYX-473 mount orchid show Mission Control routes

--- app/routers/live_mission_control.py
+++ app/routers/live_mission_control.py
@@ -9,6 +9,7 @@ from fastapi import APIRouter
 
 from app.routers.calyx_operator_chat import router as chat_router
 from app.routers.calyx_runtime_controls import create_runtime_controls_router
+from app.routers.orchid_show_operations import router as orchid_show_router
 from app.routers.volunteer_service import router as volunteer_service_router
 from app.security import verify_owner_or_api_key
 from runtime.governed_worker_loop import GovernedWorkerLoop
@@ -42,3 +43,4 @@ router.include_router(
     create_runtime_controls_router(get_runtime_controls, verify_owner_or_api_key)
 )
 router.include_router(volunteer_service_router)
+router.include_router(orchid_show_router)
