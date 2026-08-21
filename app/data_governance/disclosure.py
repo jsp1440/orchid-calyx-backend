e5a91b48723c060d96a88474be94884ee3cb82f0
Jeff Parham <jsp1440@aol.com>
2026-08-21 15:25:35 -0700

    Fix security lint imports

--- app/data_governance/disclosure.py
+++ app/data_governance/disclosure.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
 from copy import deepcopy
-from typing import Any, Mapping
+from typing import Any
 
 from .models import DataPolicyDecision, DisclosureMode
 
