ca60d5f62b91d52635626cc1743a2f93eb2bdabe
Jeff Parham <jsp1440@aol.com>
2026-09-12 13:02:57 -0700

    fix(ci): sort scientific synthesis exports

--- app/scientific_synthesis/__init__.py
+++ app/scientific_synthesis/__init__.py
@@ -3,4 +3,4 @@
 from .run_manifest import MANIFEST_VERSION, build_run_evidence_manifest
 from .service import ScientificSynthesisService
 
-__all__ = ["ScientificSynthesisService", "build_run_evidence_manifest", "MANIFEST_VERSION"]
+__all__ = ["MANIFEST_VERSION", "ScientificSynthesisService", "build_run_evidence_manifest"]
